8764a3be4a7276cf363a9a4ddd4d4b60c045ace3
aalaswad275 <aalaswad275@gmail.com>
2025-11-23 18:39:29 +0200

    first commit

---
+++ app/Http/Controllers/DepartmentController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Department;
+class DepartmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        $dept = Department::all();
+        return view('Admin.Department.index', compact('dept'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+        return view('Admin.Department.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+        $dept = Department::find($id);
+        return view('Admin.Departemnt.show',compact('dept'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+         $dept = Department::find($id);
+        return view('Admin.Departemnt.edit',compact('dept'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
