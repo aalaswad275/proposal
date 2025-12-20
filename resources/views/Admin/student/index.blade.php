ec6b5fc06cabe38b855199d5bbd136090f9fda2d
aalaswad275 <aalaswad275@gmail.com>
2025-12-20 17:22:05 +0200

    fatima

--- resources/views/Admin/student/index.blade.php
+++ resources/views/Admin/student/index.blade.php
@@ -2,6 +2,71 @@
 @section("content")
 
   <div class="container">
+     <div class="col-12">
+              <div class="card">
+                <div class="card-body">
+                  <div class="d-md-flex align-items-center">
+                    <div>
+                      <h4 class="card-title">{{__("Students List")}}</h4>
+                      <p class="card-subtitle">
+                        <a href="{{route('student.create')}}" class="btn btn-primary btn-sm float-end">{{__('Add New Student')}}</a>
+                      </p>
+                    </div>
+                    <div class="ms-auto mt-3 mt-md-0">
+                      <select class="form-select theme-select border-0" aria-label="Default select example">
+                        <option value="1">March 2025</option>
+                        <option value="2">March 2025</option>
+                        <option value="3">March 2025</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div class="table-responsive mt-4">
+                    <table class="table mb-0 text-nowrap varient-table align-middle fs-3">
+                      <thead>
+                        <tr>
+                          <th scope="col" class="px-0 text-muted">
+                            {{__('id')}}
+                          </th>
+                          <th scope="col" class="px-0 text-muted">{{__('Name')}}</th>
+                          <th scope="col" class="px-0 text-muted">
+                            {{__('Department')}}
+                          </th>
+                          <th scope="col" class="px-0 text-muted text-end">
+                            {{__('Actions')}}
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                          @foreach ($students as $student)
+                        <tr>
+                          <td class="px-0">
+                            <div class="d-flex align-items-center">
+                              <img src="{{ asset('backend/img/'.$student->image) }}" class="rounded-circle" width="40"
+                                alt="flexy" />
+                              <div class="ms-3">
+                                <h6 class="mb-0 fw-bolder">{{ $student->std_name }}</h6>
+                                <span class="text-muted"></span>
+                              </div>
+                            </div>
+                          </td>
+                          <td class="px-0">{{ $student->std_department }}</td>
+                          <td class="px-0">
+                            <span class="badge bg-info">Low</span>
+                          </td>
+                          <td class="px-0 text-dark fw-medium text-end">
+                            $3.9K
+                          </td>
+                        </tr>
+                        @endforeach
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
+
     <div class="row">
       <div class="col-md-12">
         <div class="card">
