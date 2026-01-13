bfcac1eb644530726d8a080574caa9ba561ac0fa
aalaswad275 <aalaswad275@gmail.com>
2026-01-13 20:26:56 +0200

    nice

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -5,38 +5,24 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Student;
 use App\Models\Department;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\File;
 
 class StudentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
         $students = Student::all();
         return view('admin.student.index', compact('students'));
-
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        //
-        return view('admin.student.create');
+        $departments = Department::all();
+        return view('admin.student.create', compact('departments'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
-        $requset->validate([
+        $request->validate([
             'name' => 'required',
             'std_id' => 'required',
             'std_dept' => 'required',
@@ -44,66 +30,51 @@ class StudentController extends Controller
             'std_semester' => 'required',
             'std_address' => 'required',
             'std_phone' => 'required',
-            'std_email' => 'required',
+            'std_email' => 'required|email',
             'std_supervisor' => 'required',
             'std_image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ]);
-        $image=null;
-        if($request->hasFile('std_image')){
+
+        $imageName = null;
+        if ($request->hasFile('std_image')) {
             $image = $request->file('std_image');
-            $imageName = time().'.'.$image->getClientOriginalExtension();
+            $imageName = time() . '.' . $image->getClientOriginalExtension();
             $image->move(public_path('images'), $imageName);
-            $image = $imageName;
         }
-        $student = new Student();
-        $student->name = $request->name;
-        $student->std_id = $request->std_id;
-        $student->std_dept = $request->std_dept;
-        $student->std_level = $request->std_level;
-        $student->std_semester = $request->std_semester;
-        $student->std_address = $request->std_address;
-        $student->std_phone = $request->std_phone;
-        $student->std_email = $request->std_email;
-        $student->std_supervisor = $request->std_supervisor;
-        $student->std_image = $image;
-        $student->save();
-        return redirect()->route('student.index')->with('success', 'Student created successfully.');
 
+        Student::create([
+            'name' => $request->name,
+            'std_id' => $request->std_id,
+            'std_dept' => $request->std_dept,
+            'std_level' => $request->std_level,
+            'std_semester' => $request->std_semester,
+            'std_address' => $request->std_address,
+            'std_phone' => $request->std_phone,
+            'std_email' => $request->std_email,
+            'std_supervisor' => $request->std_supervisor,
+            'std_image' => $imageName,
+        ]);
 
-
-
-
-
-
+        return redirect()->route('student.index')
+            ->with('success', 'Student created successfully.');
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
-        //
-        $student = Student::find($id);
+        $student = Student::findOrFail($id);
         return view('admin.student.show', compact('student'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(string $id)
     {
-        //
-        $student = Student::find($id);
-        return view('admin.student.edit', compact('student'));
+        $student = Student::findOrFail($id);
+        $departments = Department::all();
+        return view('admin.student.edit', compact('student', 'departments'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
-        //
-           $requset->validate([
+        $request->validate([
             'name' => 'required',
             'std_id' => 'required',
             'std_dept' => 'required',
@@ -111,51 +82,52 @@ class StudentController extends Controller
             'std_semester' => 'required',
             'std_address' => 'required',
             'std_phone' => 'required',
-            'std_email' => 'required',
+            'std_email' => 'required|email',
             'std_supervisor' => 'required',
-            'std_image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'std_image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ]);
-        $student = Student::find($id);
-        $image=$student->std_image;
-        if($request->hasFile('std_image'))
-        {
-            $image = $request->file('std_image');
-            $imageName = time().'.'.$image->getClientOriginalExtension();
-            $image->move(public_path('images'), $imageName);
-            $image = $imageName;
-            $oldpath = public_path('images/'.$student->std_image);
-            if(file_exists($oldpath)){
-                unlink($oldpath);
+
+        $student = Student::findOrFail($id);
+        $imageName = $student->std_image;
+
+        if ($request->hasFile('std_image')) {
+            if ($imageName && file_exists(public_path('images/' . $imageName))) {
+                unlink(public_path('images/' . $imageName));
             }
 
+            $image = $request->file('std_image');
+            $imageName = time() . '.' . $image->getClientOriginalExtension();
+            $image->move(public_path('images'), $imageName);
         }
 
-        $student->name = $request->name;
-        $student->std_id = $request->std_id;
-        $student->std_dept = $request->std_dept;
-        $student->std_level = $request->std_level;
-        $student->std_semester = $request->std_semester;
-        $student->std_address = $request->std_address;
-        $student->std_phone = $request->std_phone;
-        $student->std_email = $request->std_email;
-        $student->std_supervisor = $request->std_supervisor;
-        $student->std_image = $image;
-        $student->save();
-        return redirect()->route('student.index')->with('success', 'Student created successfully.');
+        $student->update([
+            'name' => $request->name,
+            'std_id' => $request->std_id,
+            'std_dept' => $request->std_dept,
+            'std_level' => $request->std_level,
+            'std_semester' => $request->std_semester,
+            'std_address' => $request->std_address,
+            'std_phone' => $request->std_phone,
+            'std_email' => $request->std_email,
+            'std_supervisor' => $request->std_supervisor,
+            'std_image' => $imageName,
+        ]);
+
+        return redirect()->route('student.index')
+            ->with('success', 'Student updated successfully.');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
-        //
-        $student = Student::find($id);
-        $oldpath = public_path('images/'.$student->std_image);
-        if(file_exists($oldpath)){
-            unlink($oldpath);
+        $student = Student::findOrFail($id);
+
+        if ($student->std_image && file_exists(public_path('images/' . $student->std_image))) {
+            unlink(public_path('images/' . $student->std_image));
         }
+
         $student->delete();
-        return redirect()->route('student.index')->with('success', 'Student deleted successfully.');
+
+        return redirect()->route('student.index')
+            ->with('success', 'Student deleted successfully.');
     }
 }
--- resources/views/Admin/student/create.blade.php
+++ resources/views/Admin/student/create.blade.php
@@ -1,40 +1,91 @@
 @extends('admin')
+
 @section('content')
 <div class="body-wrapper-inner">
-<div class="container-fluid">
-    <div class="row">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">{{ __('Add New Student') }}</div>
 
-        <div class="col-md-12 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">{{__('Add New Student')}}</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('students.store') }}">
-                       @csrf
+                    <div class="panel-body">
+                        <form method="POST"
+                              action="{{ route('students.store') }}"
+                              enctype="multipart/form-data">
+                            @csrf
 
-                        <div class="form-group">
-                            <label for="student_name" >{{_('student name')}}</label>
-                            <input type="text" name="name" id="$student_name" class="form-control" required >
-                        </div>
-                        <div class="form-group">
-                            <label for="student_image" >{{_('student image')}}</label>
-                            <input type="file" name="file" id="student_image" class="form-control" required >
-                        </div>
-                        <div class="form-group">
-                            <label for="student_department" >{{_('student department')}}</label>
-                            <input type="text" name="email" id="student_department" class="form-control" required >
-                        </div>
+                            {{-- Student Name --}}
+                            <div class="form-group">
+                                <label>{{ __('Student Name') }}</label>
+                                <input type="text" name="name" class="form-control" required>
+                            </div>
 
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-primary">
-                                {{ _('Add Student') }}
-                            </button>
-                        </div>
-                    </form>
+                            {{-- Student ID --}}
+                            <div class="form-group">
+                                <label>{{ __('Student ID') }}</label>
+                                <input type="text" name="std_id" class="form-control" required>
+                            </div>
+
+                            {{-- Department --}}
+                            <div class="form-group">
+                                <label>{{ __('Department') }}</label>
+                                <input type="text" name="std_dept" class="form-control" required>
+                            </div>
+
+                            {{-- Level --}}
+                            <div class="form-group">
+                                <label>{{ __('Level') }}</label>
+                                <input type="text" name="std_level" class="form-control" required>
+                            </div>
+
+                            {{-- Semester --}}
+                            <div class="form-group">
+                                <label>{{ __('Semester') }}</label>
+                                <input type="text" name="std_semester" class="form-control" required>
+                            </div>
+
+                            {{-- Address --}}
+                            <div class="form-group">
+                                <label>{{ __('Address') }}</label>
+                                <input type="text" name="std_address" class="form-control" required>
+                            </div>
+
+                            {{-- Phone --}}
+                            <div class="form-group">
+                                <label>{{ __('Phone') }}</label>
+                                <input type="text" name="std_phone" class="form-control" required>
+                            </div>
+
+                            {{-- Email --}}
+                            <div class="form-group">
+                                <label>{{ __('Email') }}</label>
+                                <input type="email" name="std_email" class="form-control" required>
+                            </div>
+
+                            {{-- Supervisor --}}
+                            <div class="form-group">
+                                <label>{{ __('Supervisor') }}</label>
+                                <input type="text" name="std_supervisor" class="form-control" required>
+                            </div>
+
+                            {{-- Image --}}
+                            <div class="form-group">
+                                <label>{{ __('Student Image') }}</label>
+                                <input type="file" name="std_image" class="form-control" required>
+                            </div>
+
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Add Student') }}
+                                </button>
+                            </div>
+
+                        </form>
+                    </div>
+
                 </div>
             </div>
         </div>
     </div>
 </div>
-</div>
-
 @endsection
